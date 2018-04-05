9db93671298436a0b9fac07e9ed9f212fca05b54
juhyun0 <36025326+juhyun0@users.noreply.github.com>
2018-04-05 12:03:28 +0900

    Add files via upload

---
+++ Mypaint1.py
@@ -0,0 +1,26 @@
+from tkinter import*
+def change_img():
+	path=inputBox.get()
+	img=PhotoImage(file=path)
+	imageLabel.configure(image=img)
+	imageLabel.image=img
+
+window=Tk()
+
+photo=PhotoImage(file="/home/daum/juhyun/0405.gif")
+imageLabel=Label(window,image=photo)
+imageLabel.pack()
+
+inputBox=Entry(window)
+inputBox.pack()
+
+button=Button(window,text='Submit',command=change_img)
+button.pack()
+
+window.mainloop()
+
+
+
+
+
+
